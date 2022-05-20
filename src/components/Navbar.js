41c0db965aba0c57265b08b9884b9b9eb48a2ce4
github-jademon <j2python@gmail.com>
2022-05-20 22:49:00 +0900

    change navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,15 @@
-/* This example requires Tailwind CSS v2.0+ */
+import React, { useState } from 'react';
 import { Fragment } from 'react'
 import { Link } from 'react-router-dom';
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {img1} from '../../public/logo.icon';
 
 const navigation = [
-  { icon: faHouse, to: '/', current: true },
-  { icon: faHouse, to: '/signin', current: true },
-  { icon: faHouse, to: '/product/1', current: true },
-  { icon: faHouse, to: '/list', current: true },
+  { name: "home", icon: faHouse, to: '/', current: true },
+  { name: "post", icon: faPlus, to: '/signin', current: true },
 ]
 
 function classNames(...classes) {
@@ -18,72 +17,55 @@ function classNames(...classes) {
 }
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <Disclosure as="nav" className="z-50 bg-gray-800 fixed w-full">
-      {({ open }) => (
-        <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                    alt="Workflow"
-                  />
-                  <img
-                    className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                    alt="Workflow"
-                  />
-                </div>
-                <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <Link
-                        key={item.name}
-                        to={item.to}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        <FontAwesomeIcon icon={item.icon} size="20px" />
-                      </Link>
-                    ))}
-                  </div>
+    <>
+      <Disclosure as="nav" className="z-50 sticky top-0 w-full border-b bg-white">
+        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-16">
+            <div className="inset-y-0 left-0 flex items-center sm:hidden">
+              {/* Mobile menu button*/}
+              <Disclosure.Button className={"inline-flex items-center justify-center p-2 rounded-md focus:outline-none " + (isOpen ? "text-gray-800" : "text-gray-400")}>
+                {isOpen ? (
+                  <XIcon className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                )}
+              </Disclosure.Button>
+            </div>
+            
+            {/* <div className="flex items-center sm:items-stretch sm:justify-start "> */}
+            <div className="flex items-center ">
+              <Link to='/'>
+                <img
+                  className="block h-8 w-auto"
+                  src={`${process.env.PUBLIC_URL}/logo.ico`}
+                />
+              </Link>
+            </div>
+            <div className="flex">
+              <div className="hidden sm:block items-center pr-2">
+                <div className="flex space-x-4">
+                  <Link
+                    to='/signin'
+                    className={classNames(
+                      false ? 'text-gray-700' : 'text-gray-400 hover:text-gray-700',
+                      'px-3 py-2 text-md font-semibold'
+                    )}
+                    // aria-current={true ? 'page' : undefined}
+                  >
+                    {/* <FontAwesomeIcon icon={faPlus} size="lg" /> */}
+                    POST
+                  </Link>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="bg-gray-800 p-1 text-white"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                {/* Profile dropdown */}
+              <div className="flex items-center pr-2 sm:static sm:inset-auto sm:pr-0">
                 <Menu as="div" className="ml-3 relative">
                   <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm">
-                      <span className="sr-only">Open user menu</span>
+                    <Menu.Button className="overflow-hidden rounded-full flex items-center justify-center h-8 w-8 text-sm rounded-full focus:outline-none ">
                       <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
+                        className=""
+                        src={`${process.env.PUBLIC_URL}/user/무서운이야기1.jpg`}
                       />
                     </Menu.Button>
                   </div>
@@ -99,32 +81,22 @@ const Navbar = () => {
                     <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
+                          <Link
+                            to="/mypage"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Your Profile
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
+                          <Link
+                            to="/"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Sign out
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                     </Menu.Items>
@@ -133,28 +105,33 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+        </div>
+        <Disclosure.Panel className="sm:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            <Link
+                to="/"
+                className={classNames(
+                  true ? 'text-gray-700' : 'text-gray-300 hover:bg-gray-300 hover:text-gray-700',
+                  'block px-3 py-2 rounded-md text-base font-medium'
+                )}
+                aria-current={true? 'page' : undefined}
+              >
+                HOME
+            </Link>
+            <Link
+                to="/signin"
+                className={classNames(
+                  true ? 'text-gray-700' : 'text-gray-300 hover:bg-gray-300 hover:text-gray-700',
+                  'block px-3 py-2 rounded-md text-base font-medium'
+                )}
+                aria-current={true? 'page' : undefined}
+              >
+                POST
+            </Link>
+          </div>
+        </Disclosure.Panel>
+      </Disclosure>
+    </>
   )
 }
 
