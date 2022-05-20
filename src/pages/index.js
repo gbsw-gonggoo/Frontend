7d633e082b5022bd6fe181f492475f51d5455b75
github-jademon <j2python@gmail.com>
2022-05-20 22:49:36 +0900

    add route page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as FindPw } from './FindPw';
 export { default as Mypage } from './Mypage';
 export { default as SignIn } from './SignIn';
-// export { default as SignUp } from './SignUp';
+export { default as SignUp } from './SignUp';
 export { default as ProductList } from './ProductList';
 export { default as ProductOverview } from './ProductOverview';
