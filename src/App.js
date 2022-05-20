e974a0830e9ddbedaffdd5db7bf802c11700d855
github-jademon <j2python@gmail.com>
2022-05-20 22:46:14 +0900

    modify setting routing css and add img

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component }from 'react';
 import { Route, Routes } from 'react-router-dom';
-import {SignIn, ProductList, ProductOverview} from './pages';
+import { SignIn, SignUp, FindPw, ProductList, ProductOverview, Mypage } from './pages';
 import Navbar from './components/Navbar';
 
 
@@ -8,12 +8,15 @@ import Navbar from './components/Navbar';
 class App extends Component {
   render() {
       return (
-        <div className="h-full">
-          {/* <Navbar /> */}
+        <div className="h-full relative">
+          <Navbar />
           <Routes>
             <Route exact path="/" element={<ProductList />}/>
             <Route path="/signin" element={<SignIn />}/>
+            <Route path="/signup" element={<SignUp />}/>
+            <Route path="/findpw" element={<FindPw />}/>
             <Route path="/list" element={<ProductList />}/>
+            <Route path="/mypage" element={<Mypage />}/>
             <Route path="/product/:id" element={<ProductOverview />}/>
           </Routes>
         </div>
