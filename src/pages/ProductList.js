2c9b879791b26027647ab0efafe0e810b7600d98
github-jademon <j2python@gmail.com>
2022-05-20 22:52:18 +0900

    feat top truncate and test

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Truncate from 'react-truncate';
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const products = [
   {
     id: 1,
-    name: '칠성사이다',
+    name: "칠성사이다, 30개입, 210ml",
     image: {
-      src: '/칠성사이다.png',
+      src: '/product/칠성사이다.png',
     },
     price: '15,720',
     pricePerPiece: '524',
@@ -16,21 +19,21 @@ const products = [
   },
   {
     id: 2,
-    name: '칠성사이다',
+    name: '초코에몽, 180ml, 24개',
     image: {
-      src: '/cat.jpg',
+      src: '/product/초코에몽.jpg',
     },
-    price: '15,720',
-    pricePerPiece: '524',
-    applicants: '4',
+    price: '13,680',
+    pricePerPiece: '570',
+    applicants: '1',
     recruitment: '30',
     date: '2022-04-30',
   },
   {
     id: 3,
-    name: '칠성사이다',
+    name: "칠성사이다, 30개입, 210ml",
     image: {
-      src: '/칠성사이다.png',
+      src: '/product/칠성사이다.png',
     },
     price: '15,720',
     pricePerPiece: '524',
@@ -40,9 +43,9 @@ const products = [
   },
   {
     id: 4,
-    name: '칠성사이다',
+    name: "칠성사이다, 30개입, 210ml",
     image: {
-      src: '/칠성사이다.png',
+      src: '/product/칠성사이다.png',
     },
     price: '15,720',
     pricePerPiece: '524',
@@ -52,9 +55,81 @@ const products = [
   },
   {
     id: 5,
-    name: '칠성사이다',
+    name: "칠성사이다, 30개입, 210ml",
     image: {
-      src: '/칠성사이다.png',
+      src: '/product/칠성사이다.png',
+    },
+    price: '15,720',
+    pricePerPiece: '524',
+    applicants: '4',
+    recruitment: '30',
+    date: '2022-04-30',
+  },
+  {
+    id: 6,
+    name: "칠성사이다, 30개입, 210ml",
+    image: {
+      src: '/product/칠성사이다.png',
+    },
+    price: '15,720',
+    pricePerPiece: '524',
+    applicants: '4',
+    recruitment: '30',
+    date: '2022-04-30',
+  },
+  {
+    id: 7,
+    name: "칠성사이다, 30개입, 210ml",
+    image: {
+      src: '/product/칠성사이다.png',
+    },
+    price: '15,720',
+    pricePerPiece: '524',
+    applicants: '4',
+    recruitment: '30',
+    date: '2022-04-30',
+  },
+  {
+    id: 8,
+    name: "칠성사이다, 30개입, 210ml",
+    image: {
+      src: '/product/칠성사이다.png',
+    },
+    price: '15,720',
+    pricePerPiece: '524',
+    applicants: '4',
+    recruitment: '30',
+    date: '2022-04-30',
+  },
+  {
+    id: 9,
+    name: "칠성사이다, 30개입, 210ml",
+    image: {
+      src: '/product/칠성사이다.png',
+    },
+    price: '15,720',
+    pricePerPiece: '524',
+    applicants: '4',
+    recruitment: '30',
+    date: '2022-04-30',
+  },
+  {
+    id: 10,
+    name: "칠성사이다, 30개입, 210ml",
+    image: {
+      src: '/product/칠성사이다.png',
+    },
+    price: '15,720',
+    pricePerPiece: '524',
+    applicants: '4',
+    recruitment: '30',
+    date: '2022-04-30',
+  },
+  {
+    id: 11,
+    name: "칠성사이다, 30개입, 210ml",
+    image: {
+      src: '/product/칠성사이다.png',
     },
     price: '15,720',
     pricePerPiece: '524',
@@ -66,19 +141,22 @@ const products = [
 ]
 
 const ProductList = () => {
+  const toTop = () => {
+    window.scrollTo({top:0, behavior:'smooth'});
+  }
   return (
-    <div className="bg-white">
-      <div className="max-w-2xl mx-auto px-4 pt-24 pb-10 lg:pt-30 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">공동구매 리스트</h2>
+    <div className="bg-slate-50 overflow-y-auto">
+      <div className="max-w-2xl mx-auto px-4 pb-10 pt-8 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="lg:text-[1.35rem] text-xl font-semibold tracking-tight text-gray-500">공동구매 리스트</h2>
 
         <div className="mt-6 grid gap-y-10 gap-x-6 grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <Link to={`/product/${product.id}`}>
-              <div key={product.id} className="group relative border rounded-md">
-                <div className="hover-group w-full min-h-80 bg-white p-4 border-b aspect-w-1 aspect-h-1 rounded-t-md overflow-hidden h-48 lg:h-80 lg:aspect-none">
-                  <div className="hover-text list-disc text-sm space-y-2">
-                      <ul>{product.applicants} / {product.recruitment} 개</ul>
-                      <ul>{product.date} 까지</ul>
+            <Link to={`/product/${product.id}`} key={product.id} >
+              <div className="group relative border rounded-md bg-white">
+                <div className="m hover-group w-full p-3 border-b rounded-t-md overflow-hidden h-48 lg:h-80 lg:aspect-none">
+                  <div className="hover-text list-disc font-semibold text-base text-gray-700">
+                      <ul>{product.applicants} / {product.recruitment} <span className="font-semibold text-[0.9rem]">개</span></ul>
+                      <ul>{product.date} <span className="font-semibold text-[0.9rem]">까지</span></ul>
                   </div>
                   <img
                     src={process.env.PUBLIC_URL + product.image.src}
@@ -86,14 +164,13 @@ const ProductList = () => {
                     className="hover-img w-full h-full object-center object-cover rounded-sm img-text-hover"
                   />
                 </div>
-                <div className="m-1 lg:m-2 lg:flex lg:justify-between p-3">
+                <div className="m lg:m-2 p-3">
                   <div>
-                    <h3 className="text-sm font-medium text-gray-900">
-                        <span aria-hidden="true" className="absolute inset-0" />
-                        {product.name}
-                    </h3>
+                    <Truncate lines={1} ellipsis={"..."} className="text-sm font-semibold text-gray-800">
+                      {product.name}
+                    </Truncate>
                   </div>
-                  <div className="lg:flex lg:justify-end">
+                  <div className="lg:mt-1 lg:flex lg:font-medium">
                     <p className="text-sm text-gray-500 lg:float-left lg:mr-1">{product.price}원</p>
                     <p className="text-sm text-gray-500">(개당 {product.pricePerPiece}원)</p>
                   </div>
@@ -101,6 +178,11 @@ const ProductList = () => {
               </div>
             </Link>
           ))}
+          <div className="fixed bottom-5 right-5 w-10 h-10 rounded-full bg-white z-50 border">
+            <button className="flex justify-center items-center text-gray-500 w-full h-full font-bold p-2 rounded-full" onClick={() => toTop()}>
+              <FontAwesomeIcon icon={faArrowUp} className="" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
