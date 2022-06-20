15d7f326d8514694546e35996f4f8b1c1031ad9d
github-jademon <j2python@gmail.com>
2022-06-20 17:05:22 +0900

    update route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component }from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { SignIn, SignUp, FindPw, ProductList, ProductOverview, Mypage } from './pages';
+import { SignIn, SignUp, FindPw, FindId, ProductList, ProductOverview, Mypage, Post } from './pages';
 import Navbar from './components/Navbar';
 
 
@@ -8,13 +8,16 @@ import Navbar from './components/Navbar';
 class App extends Component {
   render() {
       return (
-        <div className="h-full relative">
+        <div className="h-full relative lg:overflow-hidden">
           <Navbar />
           <Routes>
             <Route exact path="/" element={<ProductList />}/>
             <Route path="/signin" element={<SignIn />}/>
+            <Route path="/login" element={<SignIn />}/>
             <Route path="/signup" element={<SignUp />}/>
             <Route path="/findpw" element={<FindPw />}/>
+            <Route path="/findid" element={<FindId />}/>
+            <Route path="/post" element={<Post />}/>
             <Route path="/list" element={<ProductList />}/>
             <Route path="/mypage" element={<Mypage />}/>
             <Route path="/product/:id" element={<ProductOverview />}/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 export { default as FindPw } from './FindPw';
+export { default as FindId } from './FindId';
 export { default as Mypage } from './Mypage';
 export { default as SignIn } from './SignIn';
 export { default as SignUp } from './SignUp';
+export { default as Post } from './Post';
 export { default as ProductList } from './ProductList';
 export { default as ProductOverview } from './ProductOverview';
